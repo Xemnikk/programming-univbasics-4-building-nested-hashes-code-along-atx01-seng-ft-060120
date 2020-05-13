2f3b28772c0b2a5978061f876a2818d1616e8085
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-13 14:15:55 +0000

    Automatically backed up by Learn

--- spec/04_challenge_spec.rb
+++ spec/04_challenge_spec.rb
@@ -1,4 +1,5 @@
-require_relative '../lib/04_fourth_challenge.rb'require_relative 'spec_helper'
+require_relative '../lib/04_fourth_challenge.rb'
+require_relative 'spec_helper'
 
 
 describe "fourth_challenge" do
